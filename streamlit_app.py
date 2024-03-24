3c971c59de275fcb523821b8126cef635b9571d7
gonzalo711 <gonzalo@ironhacks-mbp.home>
2024-03-24 17:00:22 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ fs = gcsfs.GCSFileSystem(token=gcp_credentials)
 bucket_name = 'us-central1-airbnbcomposer-b06b3309-bucket'
 file_paths = ['data/airbnb_final_listings_2024_4.csv',
               'data/airbnb_final_listings_2024_5.csv',
-              'data/airbnb_final_listings_2024_6.csv']
+              'data/airbnb_final_listings_2024_6.csv',
+              'data/airbnb_final_listings_2024_7.csv']
 
 # Function to load data
 def load_and_merge_csv(bucket_name, file_paths):
@@ -45,15 +46,24 @@ def load_and_merge_csv(bucket_name, file_paths):
 
 #Function to clean the data
 def clean_transform_data(df):
-    df['Price'] = df['Price'].str.extract(r'€ (\d+,\d+)')[0].str.replace(',', '').astype(float)
-    df['Check_in'] = pd.to_datetime(df['Check_in'])
-    df['Check_out'] = pd.to_datetime(df['Check_out'])
+    df['Price'] = df['Price'].str.extract(r'€ (\d+,\d+|\d+)')[0].str.replace(',', '').astype(float)
+    
+    # Extract Check_in and Check_out dates from the URL column
+    df['Check_in'] = df['URL'].apply(lambda x: pd.to_datetime(parse_qs(urlparse(x).query).get('check_in', [None])[0]))
+    df['Check_out'] = df['URL'].apply(lambda x: pd.to_datetime(parse_qs(urlparse(x).query).get('check_out', [None])[0]))
+    
+    # Calculate the number of nights
     df['number_nights'] = (df['Check_out'] - df['Check_in']).dt.days
-    df['Interval'] = df.apply(lambda row: f"{row['Check_in'].strftime('%Y-%m-%d')} to {row['Check_out'].strftime('%Y-%m-%d')}", axis=1)
+    
+    # Calculate Price per night; handle NaN nights
+    df['Price_per_night'] = df.apply(lambda row: row['Price'] / row['number_nights'] if row['number_nights'] > 0 else None, axis=1)
+    
+    # Extract day of week for Check-in and Check-out
     df['Check_in_day'] = df['Check_in'].dt.dayofweek
     df['Check_out_day'] = df['Check_out'].dt.dayofweek
-    df['Price_per_night'] = df['Price'] / df['number_nights']
-    df['Period'] = df.apply(lambda row: 'Weekend' if row['Check_in_day'] >= 5 or row['Check_out_day'] >= 5 else 'Weekday', axis=1)
+    
+    # Determine if the period is a Weekend or Weekday stay
+    df['Period'] = df.apply(lambda row: 'Weekend' if 5 <= row['Check_in_day'] <= 6 or 5 <= row['Check_out_day'] <= 6 else 'Weekday', axis=1)
     return df
 
 # Load, clean, and transform data
@@ -127,50 +137,6 @@ st.download_button(
     mime='text/csv',
 )
 
-
-"""conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("us-central1-airbnbcomposer-b06b3309-bucket/myfile.csv", input_format="csv", ttl=600)
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")"""
-
-
-"""
-# Function to load and merge data from multiple CSV files
-def load_data(bucket_name, file_paths):
-    #gcp_credentials = toml.load('.streamlit/secrets.toml')['gcp_service_account']
-    gcp_credentials = st.secrets["gcp_service_account"]
-    fs = gcsfs.GCSFileSystem(token=gcp_credentials)
-    #fs = gcsfs.GCSFileSystem(project='airbnbscraper-417722', token=gcp_credentials)
-    all_data = pd.DataFrame()
-    
-    # Ensure the file paths list is not empty and contains valid file paths
-    if file_paths and all(isinstance(path, str) for path in file_paths):
-        for file_path in file_paths:
-            full_path = f"{bucket_name}/{file_path}"
-            with fs.open(full_path) as f:
-                df = pd.read_csv(f)
-                all_data = pd.concat([all_data, df], ignore_index=True)
-    return all_data
-
-
-st.write(st.secrets)
-
-
-# Load data from GCS
-bucket_name = 'us-central1-airbnbcomposer-b06b3309-bucket/data'
-file_paths = ['airbnb_final_listings_2024_4.csv', 'airbnb_final_listings_2024_5.csv', 'airbnb_final_listings_2024_6.csv']
-data = load_data(bucket_name, file_paths)
-data = clean_transform_data(data)
-
-# Streamlit UI
-st.title('Airbnb Listings Dashboard')
-month_selection = st.selectbox('Select Month', data['Check_in'].dt.month_name().unique())
-bedroom_selection = st.selectbox('Select Number of Bedrooms', data['Bedrooms'].unique())
-
-# Filter data based on selections
-filtered_data = data[(data['Check_in'].dt.month_name() == month_selection) & (data['Bedrooms'] == bedroom_selection)]
 
 # Pivot table for Livin Paris
 livin_paris_data = filtered_data[filtered_data['Livinparis'] == 'Yes']
